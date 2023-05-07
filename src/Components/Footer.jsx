02a85ccd1b1e50e6043d24e1840a0845ed037c70
Sahil Verma <sahilverma111201@gmail.com>
2023-05-07 18:14:09 +0530

    Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -18,17 +18,15 @@ const Footer = () => {
                 <hr />
                 <p className="mt-4">All rights reserved by <br /> Sahil Verma</p>
               </div>
-              <div className="col-md-4 col-lg-3 fs-2 lead">
-                <div>
-                  <ul className="d-flex justify-content-center mt-4 align-content-center flex-wrap">
-                    <li className="list-unstyled navRes mx-md-2 mx-auto mx-lg-3"><a className="text-white" href="https://bit.ly/3yL5fYY" target="_blank"><BsFacebook /></a></li>
-                    <li className="list-unstyled navRes mx-md-2 mx-auto mx-lg-3"><a className="text-white" href="https://www.linkedin.com/in/sahil-verma-6963bb229" target="_blank"><BsLinkedin /></a></li>
-                    <li className="list-unstyled navRes mx-md-2 mx-auto mx-lg-3"><a className="text-white" href="https://bit.ly/2WGjIs9" target="_blank"><BsYoutube /></a></li>
-                    <li className="list-unstyled navRes mx-md-2 mx-auto mx-lg-3"><a className="text-white" href="https://www.instagram.com/not_sahil_19" target="_blank"><BsInstagram /></a></li>
-                    <li className="list-unstyled navRes mx-md-2 mx-auto mx-lg-3"><a className="text-white" href="https://github.com/notSahilVerma" target="_blank"><BsGithub /></a></li>
-                    <li className="list-unstyled navRes mx-md-2 mx-auto mx-lg-3"><a className="text-white" href="https://bit.ly/2WSKhKx" target="_blank"><BsTwitter /></a></li>
+              <div className="col-md-4 col-lg-3 fs-2 col-11">
+                  <ul className="d-flex justify-content-center mt-4 align-content-center flex-wrap mx-auto">
+                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://bit.ly/3yL5fYY" target="_blank"><BsFacebook /></a></li>
+                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://www.linkedin.com/in/sahil-verma-6963bb229" target="_blank"><BsLinkedin /></a></li>
+                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://bit.ly/2WGjIs9" target="_blank"><BsYoutube /></a></li>
+                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://www.instagram.com/not_sahil_19" target="_blank"><BsInstagram /></a></li>
+                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://github.com/notSahilVerma" target="_blank"><BsGithub /></a></li>
+                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://bit.ly/2WSKhKx" target="_blank"><BsTwitter /></a></li>
                   </ul>
-                </div>
               </div>
             </div>
           </div>
