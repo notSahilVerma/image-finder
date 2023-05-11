2337c8f6f7bfb6896765858769e45b503d25563c
Sahil Verma <sahilverma111201@gmail.com>
2023-05-11 14:03:55 +0530

    Routes and about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,20 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <div className="min-vh-100">About</div>
+      
+  <div class="container">
+    <div class="about-section d-flex flex-column justify-content-center m-auto">
+      <h1 className="align-self-center w-100">About Me</h1>
+      <p className="col-8  align-self-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin varius semper eros, sit amet consectetur
+        adipiscing elit. Curabitur a odio felis. Suspendisse potenti. Vestibulum posuere augue eget magna interdum, at
+        consectetur neque tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+        turpis egestas.
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin varius semper eros, sit amet consectetur
+        adipiscing elit. Curabitur a odio felis. Suspendisse potenti.</p>
+    </div>
+  </div>
     </>
   );
 };
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import {BsFacebook, BsLinkedin, BsInstagram, BsYoutube, BsGithub, BsTwitter} from 'react-icons/bs';
+import {
+  BsFacebook,
+  BsLinkedin,
+  BsInstagram,
+  BsYoutube,
+  BsGithub,
+  BsTwitter,
+} from "react-icons/bs";
+import { NavLink } from "react-router-dom";
+
 
 const Footer = () => {
   return (
@@ -8,30 +17,101 @@ const Footer = () => {
         <footer className=" bg-dark text-light py-5">
           <div className="container text-center">
             <div className="row text-center d-flex justify-content-between">
-              <div className="col-md-4 col-lg-3 fs-6 fw-lighter text mt-md-4">Copyright <sup>&copy;</sup><br /> <span className="fw-lighter">Warlock Inc. {(new Date().getFullYear())}</span></div>
+              <div className="col-md-4 col-lg-3 fs-6 fw-lighter text mt-md-4">
+                Copyright <sup>&copy;</sup>
+                <br />{" "}
+                <span className="fw-lighter">
+                  Warlock Inc. {new Date().getFullYear()}
+                </span>
+              </div>
               <div className="col-md-4 col-lg-3 fs-6 lead mt-5 mt-md-0">
                 <ul className="font-monospace list-unstyled d-flex flex-row justify-content-center align-content-center">
-                  <li className="mx-auto navRes"><a className=" text-decoration-none text-white" href="/">Home</a></li>
-                  <li className="mx-auto navRes"><a className=" text-decoration-none text-white" href="/about">About</a></li>
-                  <li className="mx-auto navRes"><a className=" text-decoration-none text-white" href="/contact">Contact</a></li>
+                  <li className="nav-item mx-auto navRes">
+                    <NavLink
+                      className="nav-link navLinks lastNavlink text-white"
+                      to="/"
+                    >
+                      Home
+                    </NavLink>
+                  </li>
+                  <li className="nav-item mx-auto navRes">
+                    <NavLink
+                      className="nav-link navLinks secondNavlink text-white"
+                      to="./about"
+                    >
+                      About
+                    </NavLink>
+                  </li>
+                  <li className="nav-item mx-auto navRes">
+                    <NavLink
+                      className="nav-link navLinks thirdNavlink text-white"
+                      to="./contact"
+                    >
+                      Contact
+                    </NavLink>
+                  </li>
                 </ul>
+
                 <hr />
-                <p className="mt-4">All rights reserved by <br /> Sahil Verma</p>
+                <p className="mt-4">
+                  All rights reserved by <br /> Sahil Verma
+                </p>
               </div>
               <div className="col-md-4 col-lg-3 fs-2 col-11">
-                  <ul className="d-flex justify-content-center mt-4 align-content-center flex-wrap mx-auto">
-                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://www.facebook.com/harshwarlock" target="_blank"><BsFacebook /></a></li>
-                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://www.linkedin.com/in/sahil-verma-6963bb229" target="_blank"><BsLinkedin /></a></li>
-                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://www.youtube.com/channel/UCTpymGhlXHvgKnykQOEFusw" target="_blank"><BsYoutube /></a></li>
-                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://www.instagram.com/not_sahil_19" target="_blank"><BsInstagram /></a></li>
-                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://github.com/notSahilVerma" target="_blank"><BsGithub /></a></li>
-                    <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3"><a className="text-white" href="https://twitter.com/HarshWarlock" target="_blank"><BsTwitter /></a></li>
-                  </ul>
+                <ul className="d-flex justify-content-center mt-4 align-content-center flex-wrap mx-auto">
+                  <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3">
+                    <a
+                      className="text-white"
+                      href="https://www.facebook.com/harshwarlock"
+                    >
+                      <BsFacebook />
+                    </a>
+                  </li>
+                  <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3">
+                    <a
+                      className="text-white"
+                      href="https://www.linkedin.com/in/sahil-verma-6963bb229"
+                    >
+                      <BsLinkedin />
+                    </a>
+                  </li>
+                  <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3">
+                    <a
+                      className="text-white"
+                      href="https://www.youtube.com/channel/UCTpymGhlXHvgKnykQOEFusw"
+                    >
+                      <BsYoutube />
+                    </a>
+                  </li>
+                  <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3">
+                    <a
+                      className="text-white"
+                      href="https://www.instagram.com/not_sahil_19"
+                    >
+                      <BsInstagram />
+                    </a>
+                  </li>
+                  <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3">
+                    <a
+                      className="text-white"
+                      href="https://github.com/notSahilVerma"
+                    >
+                      <BsGithub />
+                    </a>
+                  </li>
+                  <li className="list-unstyled navRes mx-auto mx-md-2 mx-lg-3">
+                    <a
+                      className="text-white"
+                      href="https://twitter.com/HarshWarlock"
+                    >
+                      <BsTwitter />
+                    </a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
         </footer>
-
       </>
     </div>
   );
